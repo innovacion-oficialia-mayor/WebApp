49eb0e53b6ff7b899344219c569aeeceebb0ba7d
francisco-solis99 <franciscosoliscss@gmail.com>
2022-05-04 10:04:28 -0500

    [Agregar] el icono de celaya y eliminar el icono por defecto

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,11 @@
 </script>
 
 <template>
-  <h1>Hola esta es una prueba de muchas letras para ver el font que hay</h1>
   <RouterView />
 </template>
 
 <style>
+/* css base styles */
 @import '@/assets/base.css';
-h1{
-  font-size: 1rem;
-  margin: 1rem;
-}
+
 </style>
