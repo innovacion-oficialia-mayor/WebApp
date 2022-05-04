0b36423764e6c4436ba86f937c505d524d460e2f
francisco-solis99 <franciscosoliscss@gmail.com>
2022-05-04 13:02:34 -0500

    [Agregar] la vista inicial y el componente item para su llenado dinamico

---
+++ src/components/TheStartItem.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'TheStartItem',
+};
+</script>
+
+<template>
+  <div class="item__container">
+    <picture class="item__image-wrapper">
+      <slot name="image"></slot>
+    </picture>
+    <div class="start__info">
+      <slot name="info"></slot>
+    </div>
+    <div class="start__actions">
+      <slot name="actions"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/TheStartSesion.vue
@@ -0,0 +1,33 @@
+<script>
+import TheStartItem from '@/components/TheStartItem.vue';
+
+export default {
+    name: "TheStart",
+    componets: {
+        'the-start-item':TheStartItem,
+    },
+    components: { TheStartItem }
+};
+</script>
+
+<template>
+  <the-start-item>
+    <template #image>
+      <img class="start__image" src="@/assets/images/start-choose.svg" alt="Imagen de bienvenida">
+    </template>
+     <template #info>
+      <h2 class="start__title">Elige tu sesión</h2>
+      <p class="start__disclaimer">Elige sí eres administrador o servidor público para continuar</p>
+    </template>
+    <template #actions>
+      <router-link to="">Soy Administrador</router-link>
+      <router-link to="">Soy Servidor Público</router-link>
+    </template>
+    <picture>
+      <img class="start__logo" src="@/assets/logo.png" alt="Logo de Celaya">
+    </picture>
+  </the-start-item>
+</template>
+
+<style scoped>
+</style>
