371b316283c2a157ec94b271bd942bd3a79e844e
francisco-solis99 <franciscosoliscss@gmail.com>
2022-05-05 12:44:21 -0500

    [Arreglar] el componente de app link como componente global

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+// Global componentes
+import AppLink from '@/components/AppLink.vue';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.component('app-link',AppLink);
 app.mount('#app');
