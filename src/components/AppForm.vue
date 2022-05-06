3730b217b59b79aad19a4ea831c5c85fbee92809
francisco-solis99 <franciscosoliscss@gmail.com>
2022-05-06 18:03:13 -0500

    [Agregar] el elemento para mandar mensajes de error en el formulario

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -2,6 +2,9 @@
 import AppInput from "./AppInput.vue";
 export default {
   name: 'AppForm',
+  components: {
+  'app-input':AppInput
+  },
   props: {
     inputsList: {
       type: Array,
@@ -12,21 +15,20 @@ export default {
         labelText: "Default",
         name: "default",
       }]
+    },
+    state: {
+      type: Boolean,
     }
   },
-  components: {
-  'app-input':AppInput
-  },
-  methods : {
-
-  }
 };
 </script>
 
 <template>
   <form action="" class="app__form">
     <app-input class="app__form-input" v-for="input in inputsList" :key="input.id" :currentInput="input"></app-input>
-    <input type="submit" value="Iniciar sesión" class="app__form-submit">
+    <p v-show="state" class="app__form-msg">Datos invalidos, verifica los campos y vuelve a intentarlo</p>
+
+    <input type="submit" value="Iniciar sesión" class="app__form-submit" aria-label="submit">
   </form>
 </template>
 
@@ -52,6 +54,13 @@ export default {
     color: var(--color-primary-white);
     cursor: pointer;
     text-shadow: 4px 4px 12px rgba(255, 255, 255, 0.45);
+  }
+
+  .app__form-msg {
+    color: var(--color-secundary-red);
+    font-weight: 700;
+    font-size: 0.9rem;
+    text-align: left;
   }
 
 </style>
