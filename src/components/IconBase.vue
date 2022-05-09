a78bde3ae0d5759bb982849f4ee0d7e7054cd090
francisco-solis99 <franciscosoliscss@gmail.com>
2022-05-09 18:39:09 -0500

    [Agregar] el compoennte del header y del icono dinamico y sus estilos

---
+++ src/components/IconBase.vue
@@ -0,0 +1,48 @@
+<script>
+import ICONS from '@/assets/icons/icons.js';
+export default {
+  name: 'IconBase',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: 'white',
+    },
+    width: {
+      type: Number,
+      default: 24,
+    },
+    height: {
+      type: Number,
+      default: 24,
+    },
+
+    role:{
+      type: String,
+      default: 'icon',
+    }
+  },
+  computed: {
+    svgPath(){
+      return ICONS[this.name];
+    },
+    svgViewBox(){
+      return `0 0 ${this.width} ${this.height}`;
+    }
+  }
+};
+</script>
+
+<template>
+    <svg :width="width" :height="height" :viewBox="svgViewBox" fill="none" xmlns="http://www.w3.org/2000/svg" :role="role">
+      <path :d="svgPath" :fill="color"/>
+    </svg>
+</template>
+
+<style scoped>
+
+</style>
+
--- src/views/admin/DashboardAdmin.vue
+++ src/views/admin/DashboardAdmin.vue
@@ -1,11 +1,29 @@
 <script>
+import TheHeader from '@/components/TheHeader.vue';
+import IconBase from '@/components/IconBase.vue';
 export default {
+    name: "DashboardAdmin",
+    componets: {
+        "the-header": TheHeader,
+        "icon-base":IconBase
+    },
+    components: { TheHeader, IconBase }
 };
 </script>
 
 <template>
-    <h1>Dashboard</h1>
+  <the-header>
+    <template #text>
+      Adminsitración
+    </template>
+    <template #action>
+      <icon-base name="logout" color="#1B1A2F" width="30" height="30" role="button"></icon-base>
+    </template>
+  </the-header>
 </template>
 
 <style scoped>
+  svg {
+    width: 25px;
+  }
 </style>
