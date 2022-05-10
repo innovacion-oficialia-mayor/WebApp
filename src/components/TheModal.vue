ad5b8fbf6341baae525c76f6b680a995549d043a
francisco-solis99 <franciscosoliscss@gmail.com>
2022-05-10 12:11:53 -0500

    [Agregar] el componente del modal

---
+++ src/components/TheModal.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'TheModal',
+  props: {
+    srcImage: {
+      type: String,
+      default: '',
+    },
+  }
+};
+</script>
+
+<template>
+  <div class="the-modal__container">
+    <div class="the-modal__dialog">
+      <figure class="the-modal__image-wrapper">
+        <img :src="srcImage" alt="Imagen del modal" class="the-modal__image">
+      </figure>
+      <p class="the-modal__message">
+        <slot name="message"></slot>
+      </p>
+      <slot name="form"></slot>
+      <slot name="actions"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/views/admin/DashboardAdmin.vue
+++ src/views/admin/DashboardAdmin.vue
@@ -2,24 +2,36 @@
 import TheFooter from '@/components/TheFooter.vue';
 import TheHeader from '@/components/TheHeader.vue';
 import IconBase from '@/components/IconBase.vue';
+import TheModal from '@/components/TheModal.vue';
 export default {
   name: "DashboardAdmin",
   components: {
     "the-header": TheHeader,
     "icon-base":IconBase,
-    'the-footer': TheFooter
+    'the-footer': TheFooter,
+    'the-modal': TheModal,
   },
+  methods: {
+    logout(e) {
+      // do something with the store
+      // this.$store.dispatch('logout');
+      console.log(e.target);
+    }
+  }
 };
 </script>
 
 <template>
   <div class="dashboard__container">
+    <the-modal srcImage="">
+      <template #message>¿Estás seguro que deseas cerrar  tu sesión?</template>
+    </the-modal>
     <the-header>
       <template #text>
         Adminsitración
       </template>
       <template #action>
-        <icon-base name="logout" color="#1B1A2F" width="30" height="30" role="button"></icon-base>
+        <icon-base name="logout" color="#1B1A2F" width="30" height="30" role="button" @click="logout"></icon-base>
       </template>
     </the-header>
     <the-footer></the-footer>
