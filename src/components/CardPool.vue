0ea304a713816ba8a86286b8ac91e07b97fdf9f8
Astra <juanitho.666.r@gmail.com>
2022-05-10 13:41:47 -0500

    [Crear] Cards de contenido para las encuestas y asignarles estilos

---
+++ src/components/CardPool.vue
@@ -0,0 +1,96 @@
+<script>
+import AppLink from '@/components/AppLink.vue';
+export default {
+    name: 'CardPool',
+    components: {
+        'app-link': AppLink
+    },
+    props: {
+        imgpath: {
+            type: String,
+            required: true
+        },
+        active: {
+            type: String,
+
+        },
+
+    }
+}
+</script>
+
+<template>
+    <article class="card__container">
+        <figure class="card__wrapimage">
+            <img class="card__image" :src="imgpath" alt="Imagen de la encuesta">
+        </figure>
+        <div class="card__info">
+            <h2 class="card__title">
+                <slot name="title"></slot>
+            </h2>
+            <p class="card__disclaimer">
+                <slot name="disclaimer"></slot>
+            </p>
+            <app-link :to="{name: 'dashboard'}" class="access__link" :class="active" >Acceder</app-link>
+        </div>
+    </article>
+</template>
+
+<style scoped>
+    .card__container {
+        display: grid;
+        grid-template-columns: 40% 60%;
+        grid-auto-rows: minmax(auto, 170px);
+        gap: 8px;
+        justify-content: center;
+        align-items: center;
+        border-radius: 8px;
+        border: 2px solid var(--color-primary-rose);
+        padding: 14px 8px;
+    }
+
+    .card__wrapimage {
+        max-width: 200px;
+    }
+
+    .card__image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .card__info {
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    }
+
+    .card__title {
+        font-size: 1.6rem;
+        font-weight: 700;
+        line-height: 2.1rem;
+    }
+
+    .card__disclaimer {
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.65rem;
+    }
+
+    .access__link {
+        width: 30%;
+        margin-top: 9px;
+        font-size: 0.9rem;
+        font-weight: 500;
+        line-height: 1rem;
+        border-radius: 5px;
+
+    }
+
+    .no {
+        background-color: var(--color-unactive-rose);
+    }
+
+
+
+</style>
--- src/views/admin/DashboardAdmin.vue
+++ src/views/admin/DashboardAdmin.vue
@@ -2,12 +2,14 @@
 import TheFooter from '@/components/TheFooter.vue';
 import TheHeader from '@/components/TheHeader.vue';
 import IconBase from '@/components/IconBase.vue';
+import CardPool from '@/components/CardPool.vue';
 export default {
   name: "DashboardAdmin",
   components: {
     "the-header": TheHeader,
     "icon-base":IconBase,
-    'the-footer': TheFooter
+    'the-footer': TheFooter,
+    'card-pool': CardPool
   },
 };
 </script>
@@ -22,6 +24,46 @@ export default {
         <icon-base name="logout" color="#1B1A2F" width="30" height="30" role="button"></icon-base>
       </template>
     </the-header>
+
+    <section class="dashboard__content">
+
+      <card-pool imgpath = "/src/assets/images/card-1.svg" active = "yes" >
+        <template #title>
+          Registro y consulta
+        </template>
+        <template #disclaimer>
+          Registra nuevos empleados, edita, elimina y busca los empleados existentes.
+        </template>
+      </card-pool>
+
+      <card-pool imgpath = "/src/assets/images/card-2.svg" active = "yes" >
+        <template #title>
+          Clima Laboral
+        </template>
+        <template #disclaimer>
+          Agenda encuestas, administra las características y lee informes estadísticos.
+        </template>
+      </card-pool>
+
+      <card-pool imgpath = "/src/assets/images/card-3.svg" active = "no">
+        <template #title>
+          Norma 035
+        </template>
+        <template #disclaimer>
+          Disponible proximamente...
+        </template>
+      </card-pool>
+
+      <card-pool imgpath = "/src/assets/images/card-3.svg" active = "no" >
+        <template #title>
+          Evaluación de desempeño
+        </template>
+        <template #disclaimer>
+          Disponible proximamente...
+        </template>
+      </card-pool>
+    </section>
+
     <the-footer></the-footer>
   </div>
 </template>
@@ -35,4 +77,13 @@ export default {
   svg {
     width: 25px;
   }
+
+  .dashboard__content {
+    display: grid;
+    width: 93%;
+    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+    margin: 20px auto;
+    gap: 16px;
+  }
+
 </style>
